8d40f405e0ebee20a0bc003a3abbb3c8eea46fdf
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2019-02-16 13:58:08 +0200

    setup basic files WIP

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"; // import React package 
+import ReactDOM from "react-dom";// // import ReactDOM package
+import App from "./app.js";
+import "./style.css";// // import our external css file
+ReactDOM.render(
+  <App />, document.getElementById("content")
+);
